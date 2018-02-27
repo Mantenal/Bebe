2a8fc123fce0944e2332d0192ab07b88f1670d57
Unknown <carlosloco41@gmail.com>
2018-02-26 21:05:11 -0600

    App terminada
    faltan conexiones con el servidor

--- app/src/main/java/com/example/user/proyec/config.java
+++ app/src/main/java/com/example/user/proyec/config.java
@@ -1,6 +1,7 @@
 package com.example.user.proyec;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,22 +12,25 @@ import android.view.Menu;
 
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 
 
 public class config extends AppCompatActivity {
+
+    Switch switchE;
+    EditText campo1,campo2;
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-
         switch (item.getItemId()) {
 
             case R.id.esta:
@@ -48,12 +52,49 @@ public class config extends AppCompatActivity {
     }
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        switchE=findViewById(R.id.switch8);
+        campo1=findViewById(R.id.editText);
+        campo2=findViewById(R.id.editText6);
+
+        if(switchE.isChecked()){
+            campo1.setEnabled(true);
+            campo1.setCursorVisible(true);
+            campo2.setEnabled(true);
+            campo2.setCursorVisible(true);
+        }
+
+        else{
+            campo1.setEnabled(false);
+            campo1.setCursorVisible(false);
+            campo2.setEnabled(false);
+            campo2.setCursorVisible(false);
+        }
     }
 
+
+    public void onclick(View v) {
+        if (v.getId()==R.id.switch8){
+            if(switchE.isChecked()){
+                campo1.setEnabled(true);
+                campo1.setCursorVisible(true);
+                campo2.setEnabled(true);
+                campo2.setCursorVisible(true);
+            }
+
+            else{
+                campo1.setEnabled(false);
+                campo1.setCursorVisible(false);
+                campo2.setEnabled(false);
+                campo2.setCursorVisible(false);
+            }
+        }
+    }
 }
