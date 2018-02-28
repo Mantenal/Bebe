a51f135e241f7eb50cf5b649e0baa7db05482b76
Unknown <carlosloco41@gmail.com>
2018-02-27 19:31:01 -0600

    notificacion de Explocion 2.0
    Esta si es la buena

--- app/src/main/java/com/example/user/proyec/MainActivity.java
+++ app/src/main/java/com/example/user/proyec/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.user.proyec;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -51,6 +54,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        NotificationCompat.Builder mBuilder;
+
+        NotificationManager notificacion=(NotificationManager)getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
+
+        Intent i=new Intent(MainActivity.this,estad.class);
+        PendingIntent pendingintent=PendingIntent.getActivity(MainActivity.this,0,i,0);
+        mBuilder=new NotificationCompat.Builder(getApplicationContext())
+                .setContentIntent(pendingintent)
+                .setSmallIcon(R.drawable.notificacion)
+                .setContentTitle("Tu bebe va a explotar")
+                .setContentText("Ya exploto")
+                .setVibrate(new long[]{100,250,100,500})
+                .setAutoCancel(true);
+        notificacion.notify(1,mBuilder.build());
 
     }
 
