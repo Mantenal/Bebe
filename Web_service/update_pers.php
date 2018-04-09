bd029a81e0ac7fd17d2d220a21b10e1bfe9a8e7e
Unknown <carlosloco41@gmail.com>
2018-04-08 20:38:19 -0500

    webservice update y funciones pantalla
    Se agregaron los web service y mejoras a la interfaz de   configuracion

---
+++ Web_service/update_pers.php
@@ -0,0 +1,54 @@
+<?PHP
+$hostname_localhost="localhost";
+$database_localhost="simon_bebe";
+$username_localhost="Mantenal_root";
+$password_localhost="Mantenal_root";
+
+$json=array();
+
+	if(isset($_GET["id"])&& isset($_GET["ritmo_c_max"]) && isset($_GET["notificacion_s"]) && isset($_GET["notificacion_r"]) ){
+        $id=$_GET['id'];
+        $rit_max=$_GET['ritmo_c_max'];
+        $not_s=$_GET['notificacion_s'];
+        $not_r=$_GET['notificacion_r'];
+	
+		
+		$conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
+		
+    $insert="UPDATE personalizacion SET ritmo_c_max='{$rit_max}',notificacion_s='{$not_s}',notificacion_r='{$not_r}' WHERE id='{$id}'";
+        
+		$resultado_insert=mysqli_query($conexion,$insert);
+		
+		
+		
+		if($resultado_insert){
+			$consulta="SELECT * FROM personalizacion WHERE id = '{$id}'";
+			$resultado=mysqli_query($conexion,$consulta);
+			
+			if($registro=mysqli_fetch_array($resultado)){
+				$json['personalizacion'][]=$registro;
+			}
+			mysqli_close($conexion);
+			echo json_encode($json);
+		}
+		else{
+  echo($id);
+			$resulta["id"]=0;
+			$resulta["ritmo_c_max"]='No Registra';
+            $resulta["notificacion_s"]='No Registra';
+            $resulta["notificacion_r"]='No Registra';
+			$json['personalizacion'][]=$resulta;
+			echo json_encode($json);
+		}
+		
+	}
+	else{
+			$resulta["id"]=0;
+			$resulta["ritmo_c_max"]='WS No retorna';
+            $resulta["notificacion_s"]='WS No retorna';
+            $resulta["notificacion_r"]='WS No retorna';
+			$json['personalizacion'][]=$resulta;
+			echo json_encode($json);
+		}
+
+?>
--- app/src/main/java/com/example/user/proyec/config.java
+++ app/src/main/java/com/example/user/proyec/config.java
@@ -1,32 +1,47 @@
 package com.example.user.proyec;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-
-
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.CompoundButton;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
 
-public class config extends AppCompatActivity implements Response.Listener<JSONObject>,Response.ErrorListener{
-
+public class config extends AppCompatActivity implements Response.Listener<JSONObject> ,Response.ErrorListener{
+    Spinner spinner;
     Switch switchE;
-    EditText campo1,campo2;
+    EditText edit_Temp, edit_ritmo,edit_peso,edit_naci;
+    Button restaurar,guardar;
+    ProgressDialog progreso;
+    RequestQueue reques;
+    JsonObjectRequest jsonObjectRequest;
 
+    private int ruido,sueño,per,sexo=0,bebe;
+
+
+
+    /**
+     * Esta funcion  es la encargada de generar el menu superior de la aplicacion
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -34,6 +49,11 @@ public class config extends AppCompatActivity implements Response.Listener<JSONO
         return true;
     }
 
+    /**
+     * Funcion encargada de generar los botones de la parte superior y decirle la funcion que va a tomar
+     * @param item
+     * @return retorna el item que fue presionado para que se ejecute la ccion requerida
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -51,10 +71,15 @@ public class config extends AppCompatActivity implements Response.Listener<JSONO
 
     }
 
+    /**
+     * Esta funcion se encarga de abrir la pantalla de red cuando es presionado el boton correspondiente en la interfaz
+     * @param v
+     */
     public void lanzar(View v) {
         Intent i = new Intent(this, Red.class );
         startActivity(i);
     }
+
 
 
     @Override
@@ -63,43 +88,127 @@ public class config extends AppCompatActivity implements Response.Listener<JSONO
         setContentView(R.layout.activity_config);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        switchE=findViewById(R.id.switch8);
-        campo1=findViewById(R.id.editText);
-        campo2=findViewById(R.id.editText6);
+        switchE=findViewById(R.id.val_persona);
+        edit_Temp =findViewById(R.id.edit_Temp);
+        edit_ritmo =findViewById(R.id.edit_ritmo);
+        edit_peso=findViewById(R.id.edit_peso);
+        edit_naci=findViewById(R.id.edit_naci);
+        restaurar=findViewById(R.id.btn_resta);
+        guardar=findViewById(R.id.btn_guar);
+        spinner=(Spinner)findViewById(R.id.spinnerid);
+        reques= Volley.newRequestQueue(getApplicationContext());
+        bebe=15;
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cargarwebservice();
+            }
+        });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(adapterView.getContext(),"Seleccionado: " +adapterView.getItemAtPosition(i).toString(),Toast.LENGTH_SHORT).show();
+                String selec;
+                selec=adapterView.getItemAtPosition(i).toString();
+
+                 switch (selec){
+
+                     case "Femenino":
+
+                         sexo=0;
+                         Toast.makeText(config.this,"El id es "+sexo,Toast.LENGTH_SHORT).show();
+                         break;
+                     case "Masculino":
+                         sexo=1;
+                         Toast.makeText(config.this,"El id es "+ sexo,Toast.LENGTH_SHORT).show();
+                         break;
+                         default:
+                             break;
+                 }
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
         if(switchE.isChecked()){
-            campo1.setEnabled(true);
-            campo1.setCursorVisible(true);
-            campo2.setEnabled(true);
-            campo2.setCursorVisible(true);
+            per=1;
+            edit_Temp.setEnabled(true);
+            edit_Temp.setCursorVisible(true);
+            edit_ritmo.setEnabled(true);
+            edit_ritmo.setCursorVisible(true);
         }
 
         else{
-            campo1.setEnabled(false);
-            campo1.setCursorVisible(false);
-            campo2.setEnabled(false);
-            campo2.setCursorVisible(false);
+            per=0;
+            edit_Temp.setEnabled(false);
+            edit_Temp.setCursorVisible(false);
+            edit_ritmo.setEnabled(false);
+            edit_ritmo.setCursorVisible(false);
+        }
+    }
+
+
+
+    public void onclick2(View v){
+        if (v.getId()==R.id.notif_ruid) {
+            ruido=1;
+            Toast.makeText(config.this, "Activaste la Notificación de Ruido", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            ruido=0;
+            Toast.makeText(config.this, "Desactivaste la Notificación de Ruido", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    public void onclick3 (View v){
+        if(v.getId()==R.id.notifc_sueño) {
+            sueño=1;
+            Toast.makeText(config.this, "Activaste la Notificación de Sueño", Toast.LENGTH_SHORT).show();
+        }
+        else  {
+            sueño=0;
+            Toast.makeText(config.this, "Desactivaste la Notificación de Sueño", Toast.LENGTH_SHORT).show();
         }
     }
 
 
     public void onclick(View v) {
-        if (v.getId()==R.id.switch8){
+        if (v.getId()==R.id.val_persona){
             if(switchE.isChecked()){
-                campo1.setEnabled(true);
-                campo1.setCursorVisible(true);
-                campo2.setEnabled(true);
-                campo2.setCursorVisible(true);
+                Toast.makeText(config.this,"Activaste la Configuración Personalizada",Toast.LENGTH_SHORT).show();
+                per=1;
+                edit_Temp.setEnabled(true);
+                edit_Temp.setCursorVisible(true);
+                edit_ritmo.setEnabled(true);
+                edit_ritmo.setCursorVisible(true);
             }
 
             else{
-                campo1.setEnabled(false);
-                campo1.setCursorVisible(false);
-                campo2.setEnabled(false);
-                campo2.setCursorVisible(false);
+                Toast.makeText(config.this,"Desactivaste la Configuración Personalizada",Toast.LENGTH_SHORT).show();
+                per=0;
+                edit_Temp.setEnabled(false);
+                edit_Temp.setCursorVisible(false);
+                edit_ritmo.setEnabled(false);
+                edit_ritmo.setCursorVisible(false);
             }
         }
     }
+
+
+    private void cargarwebservice() {
+
+        String url="http://simon-baby.com/bebe/inicio_personalizacion.php";
+    }
+
 
     @Override
     public void onErrorResponse(VolleyError error) {
